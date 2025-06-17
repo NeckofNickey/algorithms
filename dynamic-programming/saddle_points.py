0171fffb26bc9bba981f3a1bef509710ab33036d
Nickita Yatsky <nickita.yatsky@yandex.ru>
2025-06-17 22:31:11 +0300

    fix: edit saddle_points

--- dynamic-programming/saddle_points.py
+++ dynamic-programming/saddle_points.py
@@ -1,50 +1,49 @@
 # https://informatics.msk.ru/mod/statements/view.php?id=88420&chapterid=112371#1
 
-def get_min_index_in_list(num_list):
-    
-    if not num_list:
-        return None
-    
-    
-    min_index = None
-    min_num = float('inf')
-    
-    for i in range(len(num_list)):
-        if num_list[i] < min_num:
-            min_num = num_list[i]
-            min_index = i
-    
-    return min_index
-
-
-def get_max_index_in_list(num_list):
-    
-    if not num_list:
-        return None
-    
-    
-    max_index = None
-    max_num = float('-inf')
-    
-    for i in range(len(num_list)):
-        if num_list[i] > max_num:
-            max_num = num_list[i]
-            max_index = i
-    
-    return max_index
-
 
 def get_saddle_points(m, n, matrix):
     
+    if not matrix:
+        return 0
+    
     saddle_points_list = []
     
-    min_string_list = [get_min_index_in_list(num_list) for num_list in matrix]
-    max_column_list = []
+    str_idx = 0
     
-    
-    
+    while str_idx < m - 1:
+        
+        min_num_in_str = float('inf')
+        min_num_in_str_idx = 0
+        
+        max_num_in_column = float('-inf')
+        max_num_in_column_idx = 0
+        
+        for j in range(n):
+            if matrix[str_idx][j] < min_num_in_str:
+                min_num_in_str = matrix[str_idx][j]
+                min_num_in_str_idx = j
+                
+        for i in range(m):
+            if matrix[i][min_num_in_str_idx] > max_num_in_column:
+                max_num_in_column = matrix[i][min_num_in_str_idx]
+                max_num_in_column_idx = i
+        
+        if min_num_in_str == max_num_in_column:
+            saddle_points_list.append([max_num_in_column_idx + 1, min_num_in_str_idx + 1])
+        
+        str_idx += 1
+                
 
     return saddle_points_list
+
+
+def print_all_saddle_points(saddle_points_list):
+    
+    if not saddle_points_list:
+        print('0')
+    
+    for i in range(len(saddle_points_list)):
+        print(' '.join(list(map(str, saddle_points_list[i]))))
 
 m, n = list(map(int, input().split()))
 matrix = []
@@ -52,4 +51,4 @@ matrix = []
 for _ in range(m):
     matrix.append(list(map(int, input().split())))
     
-print(' '.join(get_saddle_points(m, n, matrix)))
+print_all_saddle_points(get_saddle_points(m, n, matrix))
