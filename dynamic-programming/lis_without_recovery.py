888ff0c74303cc478f0808dcfb491bb8a66f4faf
Nickita Yatsky <nickita.yatsky@yandex.ru>
2025-06-21 15:25:56 +0300

    fix: edit lis_without_recovery

---
+++ dynamic-programming/lis_without_recovery.py
@@ -0,0 +1,22 @@
+# https://informatics.msk.ru/mod/statements/view.php?id=766&chapterid=1792#1
+# Но без восстановления 
+
+
+def get_lis_without_recovery(seq):
+    
+    if not seq:
+        return 0
+    
+    max_seq = [1] * len(seq)
+    
+    for i in range(1, len(seq)):
+        for j in range(i):
+            if seq[j] < seq[i]:
+                max_seq[i] = max(max_seq[i], max_seq[j] + 1)
+                
+    return max(max_seq)
+
+                
+n = int(input())
+seq = list(map(int, input().split()))
+print(get_lis_without_recovery(seq))
