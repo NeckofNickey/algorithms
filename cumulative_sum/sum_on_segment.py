45159691fabafdc0e9c090200af1efc333e3042d
Nickita Yatsky <nickita.yatsky@yandex.ru>
2025-07-05 12:19:25 +0300

    +

--- cumulative_sum/sum_on_segment.py
+++ cumulative_sum/sum_on_segment.py
@@ -1,11 +1,11 @@
 # https://informatics.msk.ru/mod/statements/view.php?id=67323&chapterid=2771#1
 
-# превышено максимальное время работы
+# превышено максимальное время раб
 
 def get_cumsum_list(array):
     
     n = len(array)
-     
+    
     cumsum_list = [0] * (n + 1)
 
     for i in range(n):
