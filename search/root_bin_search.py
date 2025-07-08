99d39b81b9580123be6b348b3cfe4289554fc5fa
Nickita Yatsky <nickita.yatsky@yandex.ru>
2025-07-08 13:38:06 +0300

    feat: add root_bin_search

---
+++ search/root_bin_search.py
@@ -0,0 +1,33 @@
+# https://informatics.msk.ru/mod/statements/view.php?id=3516&chapterid=3570#1
+# Деление пополам (поиск корня n-ой степени методом бинарного поиска)
+
+
+def get_root(n, num):
+    
+    if num == 1 or num == 0:
+        return num
+    
+    start_num = 0
+    end_num = num
+    
+    root = -1
+    
+    while abs(root ** n - num) > 0.000001:
+        mid_num = (start_num + end_num) / 2
+        
+        if mid_num**n == num:
+            return mid_num
+        elif mid_num**n > num:
+            root = mid_num
+            end_num = mid_num
+        else:
+            root = mid_num
+            start_num = mid_num
+            
+    return root
+        
+
+num = int(input())
+n = int(input())
+
+print(get_root(n, num))
