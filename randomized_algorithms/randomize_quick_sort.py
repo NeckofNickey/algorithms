b7b5ca533f4dfa96889ad2f79bfc301f89e008e8
Nickita Yatsky <nickita.yatsky@yandex.ru>
2025-07-09 13:43:51 +0300

    fix: randomize_quick_sort

--- randomized_algorithms/randomize_quick_sort.py
+++ randomized_algorithms/randomize_quick_sort.py
@@ -12,7 +12,7 @@ def quick_sort(array):
     
     
     # выбираем случайный индекс
-    random_index = random.randint((n - 1) // 4, 3 * (n - 1) // 4)
+    random_index = random.randint(0, n - 1)
     
     # ставим на первое место для осуществления сортировки Ломуто
     array[0], array[random_index] = array[random_index], array[0]
@@ -22,42 +22,14 @@ def quick_sort(array):
     split_index = 1
     
     for i in range(1, n):
-        if array[i] <= pivot:
+        if array[i] < pivot:
             array[i], array[split_index] = array[split_index], array[i]
             split_index += 1
             
     array[0], array[split_index - 1] = array[split_index - 1], array[0]
     
-    less_array = quick_sort(array[:split_index])
-    more_array = quick_sort(array[split_index:])
-    
-    return get_merging_sequences(less_array, more_array)
+    return quick_sort(array[:split_index - 1]) + [pivot] + quick_sort(array[split_index:])
 
-  
-def get_merging_sequences(list1, list2):
-    
-    # Осуществляем слияние
-    sorted_array = []
-    
-    list1_idx = 0
-    list2_idx = 0
-    
-    n_list1 = len(list1)
-    n_list2 = len(list2)
-    
-    while list1_idx < n_list1 and list2_idx < n_list2:
-        
-        if list1[list1_idx] < list2[list2_idx]:
-            sorted_array.append(list1[list1_idx])
-            list1_idx += 1
-        else:
-            sorted_array.append(list2[list2_idx])
-            list2_idx += 1
-            
-    sorted_array.extend(list1[list1_idx:])
-    sorted_array.extend(list2[list2_idx:])
-    
-    return sorted_array
     
 
 n = int(input())
