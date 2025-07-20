216911debaa5cc7d4a4e4912eee7776b6eae1c93
Nickita Yatsky <nickita.yatsky@yandex.ru>
2025-07-20 19:32:03 +0300

    feat: set_bit_value_to_1

---
+++ bit_operations/set_bit_value_to_1.py
@@ -0,0 +1,10 @@
+# https://informatics.msk.ru/mod/statements/view.php?id=77938&chapterid=124#1
+# Установить значение бита в 1
+
+def get_num(num, bit):
+    
+    return num | 1 << bit
+
+
+num, bit = tuple(map(int, input().split()))
+print(get_num(num, bit))
