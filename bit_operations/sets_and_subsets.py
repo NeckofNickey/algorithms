5f9852a4a94eb372e43ff05426b2e17f3115fc24
Nickita Yatsky <nickita.yatsky@yandex.ru>
2025-07-22 09:44:33 +0300

    feat: add sets_and_subsets

---
+++ bit_operations/sets_and_subsets.py
@@ -0,0 +1,21 @@
+# https://informatics.msk.ru/mod/statements/view.php?id=77938&chapterid=112595#1
+# Множества и подмножества
+
+import sys
+
+def get_pairs(n):
+    
+    pairs_list = []
+    
+    for num in range(n + 1):
+        if num & n - num == 0:
+            pairs_list.append(f'{num} {n - num}')
+    
+    return pairs_list
+
+
+n = int(input())
+
+pairs_list = get_pairs(n)
+
+sys.stdout.write('\n'.join(pairs_list))
