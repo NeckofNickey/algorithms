43a58eda1d196b8e88135720a87083a02c187635
Nickita Yatsky <nickita.yatsky@yandex.ru>
2025-07-22 16:36:30 +0300

    fix: adit drunkard_game

--- stack_and_deque/drunkard_game.py
+++ stack_and_deque/drunkard_game.py
@@ -5,6 +5,13 @@ def get_game_result(cards1: list, cards2: list) -> tuple:
     
     counter = 0
     
+    def first_wins(c1, c2):
+        if c1 == 0 and c2 == 9:
+            return True
+        if c1 == 9 and c2 == 0:
+            return False
+        return c1 > c2
+    
     while cards1 and cards2:
         
         if counter >= 10**6:
@@ -13,9 +20,9 @@ def get_game_result(cards1: list, cards2: list) -> tuple:
         c1 = cards1.pop(0)
         c2 = cards2.pop(0)
         
-        if (c1 == 0 and c2 == 9) or (c1 > c2 and not (c2 == 0 and c1 == 9)):
+        if first_wins(c1, c2):
             cards1.extend([c1, c2])
-        elif (c2 == 0 and c1 == 9) or (c2 > c1 and not (c1 == 0 and c2 == 9)):
+        else:
             cards2.extend([c2, c1])
         
         counter += 1
