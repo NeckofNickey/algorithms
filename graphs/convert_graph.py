b0d9f73c566bb6bb2d64906fd3d8568c71f4dc51
NeckofNickey <nickita.yatsky@yandex.ru>
2025-10-15 21:56:24 +0300

    feat: add convert_graph

---
+++ graphs/convert_graph.py
@@ -0,0 +1,25 @@
+# https://informatics.msk.ru/mod/statements/view.php?id=359&chapterid=465#1
+# От списка ребер к матрице смежности, неориентированный вариант
+
+import sys
+
+def convert_graps(m, edges_list):
+    
+    matrix = [[0] * m for _ in range(m)]
+
+    for i, j in edges_list:
+        matrix[i - 1][j - 1] = 1
+        matrix[j - 1][i - 1] = 1
+
+    return matrix
+
+
+data = sys.stdin.read().split()
+m = int(data[0])
+edges_num = int(data[1])
+edges_list = [[int(data[i]), int(data[i + 1])] for i in range(2, len(data), 2)]
+
+matrix = convert_graps(m, edges_list)
+
+for row in matrix:
+    print(' '.join(map(str, row)))
