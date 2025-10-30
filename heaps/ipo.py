4c715f2e9bdabf6ef19c5c37327190875b675b71
NeckofNickey <nickita.yatsky@yandex.ru>
2025-10-30 14:23:57 +0300

    feat: add new algos

---
+++ heaps/ipo.py
@@ -0,0 +1,4 @@
+# https://leetcode.com/problems/ipo/description/
+# 502. IPO
+
+# Решение на leetcode
---
+++ heaps/merge_k_sorted_lists.py
@@ -0,0 +1,4 @@
+# https://leetcode.com/problems/merge-k-sorted-lists/description/
+# 23. Merge k Sorted Lists
+
+# Решение на leetcode
